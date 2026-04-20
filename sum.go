6019d0d606d0511066c8d14d8d161ae39a3b0320
Abner Ugeda <devabnerugeda@gmail.com>
2026-04-20 14:52:17 -0300

    CI: add sonar cloud

--- sum.go
+++ sum.go
@@ -21,3 +21,31 @@ func times(a int, b int) int {
 func sumX(a int, b int) int {
 	return a + b + a
 }
+
+func sub2(a int, b int) int {
+	return a - b
+}
+
+func times3(a int, b int) int {
+	return a * b
+}
+
+func sumX4(a int, b int) int {
+	return a + b + a
+}
+
+func sumX5(a int, b int) int {
+	return a + b + a
+}
+
+func sub25(a int, b int) int {
+	return a - b
+}
+
+func times35(a int, b int) int {
+	return a * b
+}
+
+func sumX45(a int, b int) int {
+	return a + b + a
+}
